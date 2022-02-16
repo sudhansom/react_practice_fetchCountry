f332d655563b96e6c8c6819cd2b94e0829d43b93
bhuwan <bkspoudel@gmail.com>
2022-02-16 16:48:44 +0100

    created addCountry and remove country in Home and CartPage and implemented

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,7 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
 import React from "react";
+import { removeCountry } from "./redux/action";
 
 function CartPage() {
-  return <div>Cart Page</div>;
+  const dispatch = useDispatch();
+  const handleRemove = () => {
+    dispatch(removeCountry("Nepal"));
+  };
+  return (
+    <div>
+      <h1>Cart Page</h1>
+      <button onClick={handleRemove}>Remove Country</button>
+    </div>
+  );
 }
 
 export default CartPage;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { insertCountry, removeCountry } from "./redux/action";
 
 function Home() {
-  return <div>Home Page</div>;
+  const country = { name: "Nepal", population: 30 };
+  const dispatch = useDispatch();
+  const handleCart = () => {
+    dispatch(insertCountry(country));
+  };
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button onClick={handleCart}>Add Country</button>
+    </div>
+  );
 }
 
 export default Home;
