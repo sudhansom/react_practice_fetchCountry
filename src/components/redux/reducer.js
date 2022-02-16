e00170a41aaf4dfbd0a4983ed92b61092a3fce52
bhuwan <bkspoudel@gmail.com>
2022-02-16 14:48:06 +0100

    implement the logics for insert and remove countries

---
+++ src/components/redux/reducer.js
@@ -0,0 +1,34 @@
+const defaultState = {
+  cart: [],
+};
+
+const countryReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "INSERT_COUNTRY":
+      const incommingCountry = action.payload.name;
+      const ifExists = state.cart.find(
+        (country) => country.name === incommingCountry
+      );
+      if (ifExists) {
+        return state;
+      } else {
+        return {
+          ...state,
+          cart: [...state.cart, action.payload],
+        };
+      }
+
+    case "REMOVE_COUNTRY":
+      const countryName = action.payload;
+      const filteredCountries = state.cart.filter(
+        (country) => country.name !== countryName
+      );
+      return {
+        ...state,
+        cart: [...filteredCountries],
+      };
+
+    default:
+      return state;
+  }
+};
