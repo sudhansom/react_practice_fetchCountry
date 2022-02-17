8d0f02c9d1c08cee9727a46e9e5c89be7f5f8eec
bhuwan <bkspoudel@gmail.com>
2022-02-17 11:26:25 +0100

    added thunk for allCountry fetch and its action creater

--- src/components/redux/action.js
+++ src/components/redux/action.js
@@ -12,3 +12,29 @@ export const removeCountry = (countryName) => {
     payload: countryName,
   };
 };
+
+const getAllCountries = () => {
+  return async (dispatch, getState) => {
+    try {
+      const result = await fetch("https://restcountries.com/v2/all");
+      const countryList = await result.json();
+      dispatch(allCountriesSuccess(countryList));
+    } catch (err) {
+      dispatch(onFetchFail(err));
+    }
+  };
+};
+
+const allCountriesSuccess = (countryList) => {
+  return {
+    type: "SUCCESS_ALL_COUNTRIES",
+    payload: countryList,
+  };
+};
+
+const onFetchFail = (err) => {
+  return {
+    type: "FETCH_FAIL",
+    payload: err,
+  };
+};
