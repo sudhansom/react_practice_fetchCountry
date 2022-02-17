de6854cee32a9928ab2610ada1580a752d80ddb0
bhuwan <bkspoudel@gmail.com>
2022-02-17 19:06:07 +0100

    added all states in localStorage

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -9,8 +9,16 @@ const storeFactory = () => {
     composeWithDevTools(applyMiddleware(thunk))
   );
   store.subscribe(() => {
-    const cart = store.getState().cart;
+    const cart = store.getState().countryReducer.cart;
+    const countries = store.getState().countryReducer.countries;
+    const theme = store.getState().themeReducer.theme;
+    const err = store.getState().countryReducer.err;
+    const country = store.getState().countryReducer.country;
     localStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem("countries", JSON.stringify(countries));
+    localStorage.setItem("country", JSON.stringify(country));
+    localStorage.setItem("theme", theme);
+    localStorage.setItem("err", JSON.stringify(err));
   });
   return store;
 };
