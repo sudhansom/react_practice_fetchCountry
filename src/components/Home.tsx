174323e2bf4864db3c6a95f2ca34785ac18a60b9
bhuwan <bkspoudel@gmail.com>
2022-02-19 00:08:06 +0100

    displayed generic table for all countries

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,7 @@ import { insertCountry, removeCountry } from "./redux/action";
 
 import allCountries from "../components/customHooks/allCountries";
 import { Country } from "./redux/types";
+import DataTable from "./DataTable";
 import {
   TableHead,
   TableBody,
@@ -14,6 +15,20 @@ import {
   TableContainer,
   Button,
 } from "@material-ui/core";
+const columns = [
+  {
+    label: "name",
+    renderContent: (country: Country) => {
+      return <p>{country.name}</p>;
+    },
+  },
+  {
+    label: "capital",
+    renderContent: (country: Country) => {
+      return <p>{country.capital}</p>;
+    },
+  },
+];
 
 function Home() {
   const [err, countries] = allCountries();
@@ -23,54 +38,59 @@ function Home() {
     dispatch(insertCountry(country));
   };
   if (countries.length) {
-    return (
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Flag</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Capital</TableCell>
-              <TableCell>Area</TableCell>
-              <TableCell>Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {countries.map((country: Country) => {
-              return (
-                <TableRow>
-                  <TableCell>
-                    <img src={country.flag} height="30em"></img>
-                  </TableCell>
-                  <TableCell>
-                    <Link to={`/detail/${country.name}`}>{country.name}</Link>
-                  </TableCell>
-                  <TableCell>{country.capital}</TableCell>
-                  <TableCell>{country.area}</TableCell>
-                  <TableCell>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => {
-                        addToCart(country);
-                      }}
-                    >
-                      Add
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
+    return <DataTable items={countries} column={columns}></DataTable>;
   } else {
-    return <p>fetching....</p>;
+    return <p>fetching</p>;
   }
+  //   if (countries.length) {
+  //     return (
+  //       <TableContainer>
+  //         <Table>
+  //           <TableHead>
+  //             <TableRow>
+  //               <TableCell>Flag</TableCell>
+  //               <TableCell>Name</TableCell>
+  //               <TableCell>Capital</TableCell>
+  //               <TableCell>Area</TableCell>
+  //               <TableCell>Action</TableCell>
+  //             </TableRow>
+  //           </TableHead>
+  //           <TableBody>
+  //             {countries.map((country: Country) => {
+  //               return (
+  //                 <TableRow>
+  //                   <TableCell>
+  //                     <img src={country.flag} height="30em"></img>
+  //                   </TableCell>
+  //                   <TableCell>
+  //                     <Link to={`/detail/${country.name}`}>{country.name}</Link>
+  //                   </TableCell>
+  //                   <TableCell>{country.capital}</TableCell>
+  //                   <TableCell>{country.area}</TableCell>
+  //                   <TableCell>
+  //                     <Button
+  //                       variant="contained"
+  //                       color="primary"
+  //                       onClick={() => {
+  //                         addToCart(country);
+  //                       }}
+  //                     >
+  //                       Add
+  //                     </Button>
+  //                   </TableCell>
+  //                 </TableRow>
+  //               );
+  //             })}
+  //           </TableBody>
+  //         </Table>
+  //       </TableContainer>
+  //     );
+  //   } else {
+  //     return <p>fetching....</p>;
+  //   }
+  // }
+
+  //<button onClick={handleCart}>Add Country</button>
+  //<Link to={`/cart/${country.name}`}>{country.name}</Link>
 }
-
 export default Home;
-
-//<button onClick={handleCart}>Add Country</button>
-//<Link to={`/cart/${country.name}`}>{country.name}</Link>
